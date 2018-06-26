0fc0c9886df20a7ac15a9e7390e757c3d92e0181
zhengxinghuang <583584115@qq.com>
2018-06-27 00:17:39 +0800

    完善工程
    1、实现品牌列表查询功能
    2、实现品牌列表分页查询功能
    3、实现新增品牌功能
    4、实现修改品牌功能
    5、实现删除品牌功能
    6、实现品牌条件查询功能

--- zxhShop/zxhShop-manage-web/src/main/java/com/zxhShop/manage/controller/BrandController.java
+++ zxhShop/zxhShop-manage-web/src/main/java/com/zxhShop/manage/controller/BrandController.java
@@ -3,10 +3,9 @@ package com.zxhShop.manage.controller;
 import com.alibaba.dubbo.config.annotation.Reference;
 import com.zxhShop.pojo.TbBrand;
 import com.zxhShop.sellergoods.service.BrandService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import com.zxhShop.vo.PageResult;
+import com.zxhShop.vo.Result;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -34,5 +33,59 @@ public class BrandController {
     public List<TbBrand> findAll() {
 //        return brandService.queryAll();
         return brandService.findAll();
+    }
+
+    @GetMapping("/findPage")
+    public PageResult findPage(@RequestParam(value = "page",defaultValue = "1") Integer page,
+                               @RequestParam(value = "rows",defaultValue = "10") Integer rows) {
+        return brandService.findPage(page, rows);
+    }
+
+    @PostMapping("/add")
+    public Result add(@RequestBody TbBrand brand) {
+
+        try {
+            brandService.add(brand);
+            return Result.ok("新增成功");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return Result.fail("新增失败");
+    }
+
+    @GetMapping("/findOne")
+    public TbBrand findOne(Long id) {
+        return brandService.findOne(id);
+    }
+
+    @PostMapping("/update")
+    public Result update(@RequestBody TbBrand brand) {
+        try {
+            brandService.update(brand);
+            return Result.ok("修改成功");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return Result.fail("修改失败");
+    }
+
+    @GetMapping("/delete")
+    public Result delete(Long[] ids) {
+        try {
+            brandService.deleteByIds(ids);
+            return Result.ok("删除成功");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return Result.fail("删除失败");
+    }
+
+    @PostMapping("/search")
+    public PageResult search(@RequestBody TbBrand brand,
+                             @RequestParam(value = "page", defaultValue = "1") Integer page,
+                             @RequestParam(value = "rows", defaultValue = "10") Integer rows) {
+        return brandService.search(brand, page, rows);
     }
 }
--- zxhShop/zxhShop-sellergoods/zxhShop-sellergoods-interface/src/main/java/com/zxhShop/sellergoods/service/BrandService.java
+++ zxhShop/zxhShop-sellergoods/zxhShop-sellergoods-interface/src/main/java/com/zxhShop/sellergoods/service/BrandService.java
@@ -2,6 +2,7 @@ package com.zxhShop.sellergoods.service;
 
 import com.zxhShop.pojo.TbBrand;
 import com.zxhShop.service.BaseService;
+import com.zxhShop.vo.PageResult;
 
 import java.util.List;
 
@@ -9,4 +10,6 @@ public interface BrandService extends BaseService<TbBrand>{
     public List<TbBrand> queryAll();
 
     List<TbBrand> testPage(Integer page, Integer rows);
+
+    PageResult search(TbBrand brand, Integer page, Integer rows);
 }
